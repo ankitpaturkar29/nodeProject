0a9a1d3c43b66b912f6f1645c3ddf77f85e80889
Ankit <ankitpaturkar@gmail.com>
2023-12-24 23:16:26 +0530

    .env file added with port details

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const Users = require('./models/userActions');
 const app = express();
-const port = 3000;
+const dotenv = require('dotenv');
+dotenv.config();
+const port = process.env.PORT;
 
 app.get('/', (req, res) => {
   res.send('get all users!');
