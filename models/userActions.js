8a71057e7af61536bc43f8dbe54b70db9c0d5f13
Ankit <ankitpaturkar@gmail.com>
2023-12-24 22:34:33 +0530

    added single user get , delete and update

--- models/userActions.js
+++ models/userActions.js
@@ -15,7 +15,37 @@ async function getAllUsers(){
     return userDb('users');
 }
 
+// FUNCTION TO GET SINGLE USER
+
+async function getSingleUser(id){
+    return userDb('users')
+    .where({id: id})                
+    .first();
+}
+
+// FUNCTION TO DELETE SINGLE USER
+
+async function deleteSingleUser(id){
+    return userDb('users')
+    .where({id: id})                        
+    .del();
+}
+
+// FUNCTION TO UPDATE SINGLE USER
+
+async function singleUpdateUser(id, changes){
+    return userDb('users')
+    .where({id: id})                        
+    .update(changes)
+    .then(()=>{
+        return getSingleUser(id);
+    })
+}
+
 module.exports ={
     createUser,
-    getAllUsers
+    getAllUsers,
+    getSingleUser,
+    deleteSingleUser,
+    singleUpdateUser
 }
